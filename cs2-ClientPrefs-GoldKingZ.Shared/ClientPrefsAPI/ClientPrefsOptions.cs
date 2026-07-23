6f6e378703e87b6c5cc2bfd9c913f1154f672feb
Gold KingZ <48490385+oqyh@users.noreply.github.com>
2026-07-23 06:40:29 +0400

    1.0.2

--- cs2-ClientPrefs-GoldKingZ.Shared/ClientPrefsAPI/ClientPrefsOptions.cs
+++ cs2-ClientPrefs-GoldKingZ.Shared/ClientPrefsAPI/ClientPrefsOptions.cs
@@ -11,17 +11,13 @@ public sealed class ClientPrefsOptions
 {
     public PrefsAPI_SaveMode PrefsAPI_CookiesEnable { get; set; } = PrefsAPI_SaveMode.Disabled;
     public int PrefsAPI_CookiesAutoRemoveInactivePlayersOlderThanDays { get; set; } = 7;
-
     public PrefsAPI_SaveMode PrefsAPI_MySqlEnable { get; set; } = PrefsAPI_SaveMode.Disabled;
     public int PrefsAPI_MySqlConnectionTimeout { get; set; } = 30;
     public int PrefsAPI_MySqlRetryAttempts     { get; set; } = 3;
     public int PrefsAPI_MySqlRetryDelay        { get; set; } = 2;
     public int PrefsAPI_MySqlAutoRemoveInactivePlayersOlderThanDays { get; set; } = 7;
-
     public MySqlConfig PrefsAPI_MySqlConfig { get; set; } = new();
-
-    public string? PrefsAPI_MySqlTableName { get; set; }
-
+    public string? PrefsAPI_TableName { get; set; }
     public bool PrefsAPI_ReloadOnReconnect { get; set; } = false;
     public bool PrefsAPI_LoadDefaultAfterDrop { get; set; } = true;
     public bool PrefsAPI_DebugEnable { get; set; } = false;
