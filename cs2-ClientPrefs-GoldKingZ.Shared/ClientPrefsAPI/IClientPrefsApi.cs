9c952b698a35e876a7d8ed8813d370df6f1f0608
Gold KingZ <48490385+oqyh@users.noreply.github.com>
2026-08-26 22:47:16 +0400

    Delete cs2-ClientPrefs-GoldKingZ.Shared/ClientPrefsAPI directory

--- cs2-ClientPrefs-GoldKingZ.Shared/ClientPrefsAPI/IClientPrefsApi.cs
+++
@@ -1,8 +0,0 @@
-using CounterStrikeSharp.API.Core;
-
-namespace ClientPrefs_GoldKingZ.Shared;
-
-public interface IClientPrefsApi
-{
-    IPrefsStore<T> CreatePrefs<T>(BasePlugin plugin, ClientPrefsOptions options) where T : class, new();
-}
